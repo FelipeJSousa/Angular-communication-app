3c78c39a500ef779a32778aa42ff1c7cc725d37b
Felipe Sousa <csfelipe8@gmail.com>
2021-03-26 20:22:31 -0300

    View Child implemented

--- src/app/parent-child/parent-child.component.ts
+++ src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild(TimerComponent)
+  timer!: TimerComponent;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  start(){
+    this.timer.start();
+  }
+
+  stop(){
+    this.timer.stop();
+  }
+
+  clear(){
+    this.timer.clear();
+  }
+
+
 
 }
