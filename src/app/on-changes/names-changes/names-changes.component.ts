351838a1df63c5f230c34fbe7d97aeebc8655655
Felipe Sousa <csfelipe8@gmail.com>
2021-03-26 20:28:25 -0300

    Components created

---
+++ src/app/on-changes/names-changes/names-changes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-names-changes',
+  templateUrl: './names-changes.component.html',
+  styleUrls: ['./names-changes.component.css']
+})
+export class NamesChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
